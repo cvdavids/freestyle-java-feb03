215fa637e01d27bbfce57f3559026ed20c0358f1
Conrad Dav <conrad@gmail.com>
2024-02-04 15:06:36 +0000

    Update to test Poll SCM

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 	
-		System.out.println("Hello, world! hi Conrad");
+		System.out.println("Hello, world! hi Conrad 2nd Update");
 	}
 }
